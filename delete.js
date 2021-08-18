9784193b4512c9a3d04b03d39ec9c811a204f7e8
Tommy Aji Prabowo <44915043+tommyajip@users.noreply.github.com>
2021-08-18 13:02:42 +0700

    first commit - post test ORM

--- delete.js
+++ delete.js
@@ -1,11 +1,11 @@
 const { books } = require('./models')
 
 const query = {
-    where: { id: 2 }
+    where: { id: 1 }
 }
 books.destroy(query)
     .then(() => {
-        console.log(`Buku dengan id 2 berhasil dihapus`)
+        console.log(`Buku dengan id 1 berhasil dihapus`)
         process.exit()
     })
     .catch(err => {
